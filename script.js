d165d0a52ba3123579519bf6eb4e86368b703ad1
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-01-18 20:37:40 +0100

    Play on round of the game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getComputerChoice() {      // Generates random number for the computer
     let randNum = Math.floor(Math.random() * 3);
     let computerChoice;
 
@@ -17,4 +17,33 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice())
+
+function gameRound(playerSelection, computerSelection) {  // Plays one round of the game
+    let result;
+
+    switch (playerSelection, computerSelection) {
+        case ("Rock", "Scissors"):
+        case ("Paper", "Rock"):
+        case ("Scissors", "Paper"):
+            result = "You Win! " + `${playerSelection} beats ${computerSelection}.`;
+            break;
+
+        case ("Rock", "Rock"):
+        case ("Paper", "Paper"):
+        case ("Scissors", "Scissors"):
+            result = "Tie.";
+            break;
+
+        case ("Rock", "Paper"):
+        case ("Paper", "Scissors"):
+        case ("Scissors", "Rock"):
+            result = "You Lose. " + `${computerSelection} beats ${playerSelection}.`;
+            break;
+    }
+
+    return result;
+}
+
+let playerChoice = "Rock";
+
+console.log(gameRound(playerChoice, getComputerChoice())); // Returns the winner
