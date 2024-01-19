335dbdcd8e7c541b4d399efc3a05c12876b23a72
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-01-19 21:30:58 +0100

    Make user input case insensitive

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ function getComputerChoice() {      // Generates random number for the computer
 
     switch (randNum) {
         case 0:
-            computerChoice = "Rock";
+            computerChoice = "ROCK";
             break;
         case 1:
-            computerChoice = "Paper";
+            computerChoice = "PAPER";
             break;
         case 2:
-            computerChoice = "Scissors";
+            computerChoice = "SCISSORS";
             break;
     }
 
@@ -23,23 +23,23 @@ function getComputerChoice() {      // Generates random number for the computer
 function gameRound(playerSelection, computerSelection) {    // Plays one round of the game
     let message;
 
-    if (playerSelection === "Rock" && computerSelection === "Scissors" ||
-        playerSelection === "Paper" && computerSelection === "Rock" ||
-        playerSelection === "Scissors" && computerSelection === "Paper") {
+    if (playerSelection === "ROCK" && computerSelection === "SCISSORS" ||
+        playerSelection === "PAPER" && computerSelection === "ROCK" ||
+        playerSelection === "SCISSORS" && computerSelection === "PAPER") {
         
         message = "You Win! " + `${playerSelection} beats ${computerSelection}.`;
         roundResult = "Won";
 
-    } else if (playerSelection === "Rock" && computerSelection === "Rock" ||
-        playerSelection === "Paper" && computerSelection === "Paper" ||
-        playerSelection === "Scissors" && computerSelection === "Scissors") {
+    } else if (playerSelection === "ROCK" && computerSelection === "ROCK" ||
+        playerSelection === "PAPER" && computerSelection === "PAPER" ||
+        playerSelection === "SCISSORS" && computerSelection === "SCISSORS") {
 
         message = "Tie.";
         roundResult = "Tie";
 
-    } else if (playerSelection === "Rock" && computerSelection === "Paper" ||
-        playerSelection === "Paper" && computerSelection === "Scissors" ||
-        playerSelection === "Scissors" && computerSelection === "Rock") {
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER" ||
+        playerSelection === "PAPER" && computerSelection === "SCISSORS" ||
+        playerSelection === "SCISSORS" && computerSelection === "ROCK") {
 
         message = "You Lose. " + `${computerSelection} beats ${playerSelection}.`;
         roundResult = "Lost";
@@ -60,11 +60,11 @@ function game() {
     let ties = 0;
 
     for (let i = 0; i < 5; i++) {
-        playerChoice = prompt("Choose - Rock, Paper or Scissors (case sensitive): ");      // Prompts user for their choice
+        playerChoice = prompt("Choose - Rock, Paper or Scissors: ").toUpperCase();      // Prompts user for their choice
         
 
-        while (playerChoice !== "Rock" && playerChoice !== "Paper" && playerChoice !== "Scissors") {
-            playerChoice = prompt("Choose - 'Rock', 'Paper' or 'Scissors' (CASE SENSITIVE): ");
+        while (playerChoice !== "ROCK" && playerChoice !== "PAPER" && playerChoice !== "SCISSORS") {
+            playerChoice = prompt("Choose - 'Rock', 'Paper' or 'Scissors': ").toUpperCase();
         }
 
         console.log(gameRound(playerChoice, getComputerChoice()));      // Prints the result
